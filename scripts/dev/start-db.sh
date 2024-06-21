abdaf2c2cf0ef30cab981f84f22a74247f59d003
Manu Labricole <manulabricole@MacBook-Pro-de-Manu.local>
2024-06-21 18:26:49 +0200

    remove

--- scripts/dev/start-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "🚀 Starting the database container..."
-
-# Start the database container
-docker-compose -f docker-compose.dev.yml up -d db
-
-echo "⌛ Waiting for the database to be ready..."
-docker-compose -f docker-compose.dev.yml run --rm db ./usr/src/app/scripts/wait-for-it.sh db:5432 --timeout=60 --strict -- echo "Database is ready"
-echo "✅ Database is ready."
