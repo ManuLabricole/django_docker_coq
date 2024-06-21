74d119c0261a7689a4af0b4d4d3f40b322baa128
Manu Labricole <manulabricole@MacBook-Pro-de-Manu.local>
2024-06-21 17:29:16 +0200

    create models

--- app_core/app_2/models.py
+++ app_core/app_2/models.py
@@ -1,2 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
 # Create custome user profile
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    pincode = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.user.username)
