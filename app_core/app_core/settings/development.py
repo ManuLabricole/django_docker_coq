e13f909a8a61191ece99f96b29126c427cd70022
Manu Labricole <manulabricole@MacBook-Pro-de-Manu.local>
2024-06-21 02:04:42 +0200

    set dev value

--- app_core/app_core/settings/development.py
+++ app_core/app_core/settings/development.py
@@ -1,7 +1,9 @@
 from .base import *
 
 DEBUG = True
-
 ALLOWED_HOSTS = ["*"]
+SECRET_KEY = NotImplemented
 
-# Additional development settings
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
